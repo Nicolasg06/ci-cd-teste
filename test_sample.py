942005c3355edfaa435bdc9942b007dfd471f18b
Nicolas Goulart <nicolasgoulartf@gmail.com>
2025-11-05 15:12:10 -0300

    Atualiza workflow e adiciona arquivo de teste

--- test_sample.py
+++ test_sample.py
@@ -1,2 +1,11 @@
 def test_soma():
   assert 1 + 1 == 2
+def test_soma():
+    assert 2 + 2 == 4
+
+def test_subtracao():
+    assert 10 - 5 == 5
+
+def test_mensagem():
+    mensagem = "Hello CI/CD!"
+    assert "CI/CD" in mensagem
